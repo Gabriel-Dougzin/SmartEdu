b96a2c7ab4e762ec73bd3a37f1c4afc15aacc75c
Rebeca_Azvdd <rebecazevedo2006@gmail.com>
2024-09-19 20:45:01 -0300

    projeto pronto

---
+++ smartEdu/src/test/java/com/api/SmartEdu/SmartEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.SmartEdu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
